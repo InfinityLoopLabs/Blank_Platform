8bb08b8c37dcdcdfc877a6fe3862dea51a103a19
ko <ko.kovalev@yadro.com>
2026-04-05 22:11:47 +0300

    add nest

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,6 +1,7 @@
 import path from "node:path";
 import { parseArgs } from "./args";
 import { loadProjectConfig } from "./config";
+import { createInitConfigFile } from "./init";
 import { runCommandByKey } from "./runtime";
 import type { Variables } from "./types";
 import { builtinPlugins } from "./plugins";
@@ -8,9 +9,11 @@ import { builtinPlugins } from "./plugins";
 export * from "./types";
 export * from "./args";
 export * from "./config";
+export * from "./init";
 export * from "./runtime";
 export * from "./plugins";
 export * from "./plugins/add";
+export * from "./plugins/download";
 export * from "./plugins/insert";
 export * from "./plugins/remove-line";
 export * from "./plugins/remove";
@@ -18,6 +21,7 @@ export * from "./plugins/remove";
 export async function runCli(args: string[]): Promise<number> {
   if (args.includes("--help") || args.includes("-h")) {
     console.log("Usage: ill <commandKey> [--name <value>] [--config <path>] [--cwd <path>]");
+    console.log("Init:  ill init [--repo <owner/repo|url>] [--ref <branch|tag>] [--force]");
     console.log("Auto config names: infinityloop.config.js|mjs|cjs");
     console.log("Example: ill createWidget --name=Popup");
     return 0;
@@ -25,6 +29,17 @@ export async function runCli(args: string[]): Promise<number> {
 
   try {
     const options = parseArgs(args);
+    if (options.commandKey === "init") {
+      const { configPath } = await createInitConfigFile({
+        cwd: options.cwd,
+        repo: options.repo,
+        ref: options.ref,
+        force: options.force,
+      });
+      console.log(`Created config: ${path.relative(options.cwd, configPath)}`);
+      return 0;
+    }
+
     const { config, configPath } = await loadProjectConfig(options.cwd, options.configPath);
     const variables: Variables = {
       name: options.name,
--- cli/src/plugins/index.ts
+++ cli/src/plugins/index.ts
@@ -1,7 +1,8 @@
 import type { CommandPlugin } from "../types";
 import { addPlugin } from "./add";
+import { downloadPlugin } from "./download";
 import { insertPlugin } from "./insert";
 import { removeLinePlugin } from "./remove-line";
 import { removePlugin } from "./remove";
 
-export const builtinPlugins: CommandPlugin[] = [addPlugin, insertPlugin, removeLinePlugin, removePlugin];
+export const builtinPlugins: CommandPlugin[] = [addPlugin, downloadPlugin, insertPlugin, removeLinePlugin, removePlugin];
